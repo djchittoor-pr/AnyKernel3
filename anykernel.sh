198042d6e1180ecdadb9747a381651ee0b54040a
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2021-05-06 12:09:23 +0530

    anykernel: Initial Adaptation for Nubia Mini 5G (TP1803)
    Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,19 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=dan-kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=TP1803
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -61,4 +57,3 @@ append_file fstab.tuna "usbdisk" fstab;
 
 write_boot;
 ## end install
-
